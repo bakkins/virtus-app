54c6fb3f7722cb625894741931270bd1c00fd810
bakkins <llaurisb07@gmail.com>
2026-09-10 13:11:12 +0300

    finished exercise_widget

--- lib/models/workout_set.dart
+++ lib/models/workout_set.dart
@@ -1,10 +1,12 @@
 class WorkoutSet {
   double weight;
+  int sets;
   int reps;
   int rir;
 
   WorkoutSet({
     required this.weight,
+    required this.sets,
     required this.reps,
     required this.rir,
   });
--- lib/widgets/exercise_widget.dart
+++ lib/widgets/exercise_widget.dart
@@ -6,7 +6,27 @@ class ExerciseWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: double.infinity,
+      height: 100,
+      margin: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey[900],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Bench Press"),
+
+            const SizedBox(height: 10),
+
+            const Text("3 sets × 10 reps × 60 kg"),
+          ],
+        ),
+      ),
     );
   }
 }
