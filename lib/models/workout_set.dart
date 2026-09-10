cfe8d3ba028194888add7ac98e57a3fce088033b
bakkins <llaurisb07@gmail.com>
2026-09-10 13:21:05 +0300

    finished an simple workout_screen and added to it exercise_widget

--- lib/models/workout_set.dart
+++ lib/models/workout_set.dart
@@ -1,12 +1,10 @@
 class WorkoutSet {
   double weight;
-  int sets;
   int reps;
   int rir;
 
   WorkoutSet({
     required this.weight,
-    required this.sets,
     required this.reps,
     required this.rir,
   });
--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/widget_previews.dart'; // tikai prieks preview 
+import 'package:flutter/widget_previews.dart'; // tikai prieks preview
 import 'package:flutter/material.dart';
+import '../widgets/exercise_widget.dart';
 
 class WorkoutScreen extends StatelessWidget {
-  @Preview() // tikai prieks preview 
+  @Preview() // tikai prieks preview
   const WorkoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      title: const Text("Workout"),
+      appBar: AppBar(title: const Text("Workout")),
+      body: const ExerciseWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Add exercise");
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
