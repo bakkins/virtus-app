2268fdeaac09bae59a4e04bfa12dfc308ddc4c63
bakkins <llaurisb07@gmail.com>
2026-09-10 13:00:27 +0300

    started exercise_widget.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,14 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Text('Hello World!'),
-//         ),
-//       ),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
 }
